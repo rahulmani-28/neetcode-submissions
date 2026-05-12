09b144d5723cbe9638c409248778ab4325cdd74d
rahulmani-28 <157208622+rahulmani-28@users.noreply.github.com>
2026-05-12 20:37:56 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = -1;
+        }
+        return dfs(n, 0);
+    }
+
+    public int dfs(int n, int i) {
+        if (i >= n) return i == n ? 1 : 0;
+        if (dp[i] != -1) return dp[i];
+        return dp[i] = dfs(n, i + 1) + dfs(n, i + 2);
+    }
+}
